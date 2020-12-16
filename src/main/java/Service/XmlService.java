46c74b162cfdf5e89ca5f56941be922cd49f5308
ACXap <acxap@list.ru>
2020-12-16 15:48:26 +0700

    optim

--- src/main/java/Service/XmlService.java
+++ src/main/java/Service/XmlService.java
@@ -1,10 +1,18 @@
 package Service;
 
-import Helpers.XmlWhitespaceModule;
 import Interfaces.IXmlService;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.deser.BeanDeserializerModifier;
+import com.fasterxml.jackson.databind.deser.std.CollectionDeserializer;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.type.CollectionType;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.io.IOException;
+import java.util.Collection;
 
 public class XmlService implements IXmlService {
 
@@ -34,4 +42,52 @@ public class XmlService implements IXmlService {
         return xmlMapper;
     }
     //endregion PrivateMethod
+
+    public static class XmlWhitespaceModule extends SimpleModule {
+        private static class CustomizedCollectionDeserialiser extends CollectionDeserializer {
+
+            public CustomizedCollectionDeserialiser(CollectionDeserializer src) {
+                super(src);
+            }
+
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public Collection<Object> deserialize(JsonParser jp, DeserializationContext ctxt)
+                    throws IOException {
+                if (jp.getCurrentToken() == JsonToken.VALUE_STRING
+                        && jp.getText().matches("^[\\r\\n\\t ]+$")) {
+                    return (Collection<Object>) _valueInstantiator.createUsingDefault(ctxt);
+                }
+                return super.deserialize(jp, ctxt);
+            }
+
+            @Override
+            public CollectionDeserializer createContextual(DeserializationContext ctxt,
+                                                           BeanProperty property) throws JsonMappingException {
+                return new XmlWhitespaceModule.CustomizedCollectionDeserialiser(super.createContextual(ctxt, property));
+            }
+        }
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public void setupModule(SetupContext context) {
+            super.setupModule(context);
+            context.addBeanDeserializerModifier(new BeanDeserializerModifier() {
+                @Override
+                public JsonDeserializer<?> modifyCollectionDeserializer(
+                        DeserializationConfig config, CollectionType type,
+                        BeanDescription beanDesc, JsonDeserializer<?> deserializer) {
+                    if (deserializer instanceof CollectionDeserializer) {
+                        return new XmlWhitespaceModule.CustomizedCollectionDeserialiser(
+                                (CollectionDeserializer) deserializer);
+                    } else {
+                        return super.modifyCollectionDeserializer(config, type, beanDesc,
+                                deserializer);
+                    }
+                }
+            });
+        }
+    }
 }
